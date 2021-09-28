3f9d225bba5f3e531326fbd7158d0bca1ee4b904
Bingwenlin <50825119+Bingwenlin@users.noreply.github.com>
2021-09-28 16:34:12 +0800

    Update aliyuntest.py

--- aliyuntest.py
+++ aliyuntest.py
@@ -15,7 +15,7 @@ import requests
 import time
 
 def wechatwork(content):
-    webhook = ""
+    webhook = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"         #填入告警的入口
     header = {
         "Content-Type": "application/json",
         "Charset": "UTF-8"
@@ -59,89 +59,12 @@ def check_account():
         alyun3 = str(alyun2)
         test = '当前余额：'
         sss = '当月消费：'
-        txt3 = '阿里云【至真信息科技】账号余额'
-        txt3 = txt3 + ":\n" + test + alyun  + "\n" + sss +alyun3
-        return txt3
-
-
-def check_account2():
-    yymm = (time.strftime("%Y-%m", time.localtime()))
-    region_provider.add_endpoint('BssOpenApi', 'cn-hangzhou', 'business.aliyuncs.com')
-    clt = client.AcsClient('###',**','hangzhou')
-    request = QueryAccountBalanceRequest.QueryAccountBalanceRequest()
-    request2 = QueryMonthlyBillRequest()
-    request.set_accept_format("JSON")
-    request2.set_accept_format("JSON")
-    request2.set_BillingCycle(yymm)
-    result = clt.do_action_with_exception(request)
-    result2 = clt.do_action_with_exception(request2)
-    res_json = json.loads(str(result, encoding="utf-8"))
-    res_json2 = json.loads(str(result2, encoding="utf-8"))
-
-    if res_json["Code"] == "200":
-        alyun = res_json["Data"]["AvailableAmount"]
-        alyun2 = res_json2["Data"]["NewInvoiceAmount"]
-        alyun3 = str(alyun2)
-        test = '当前余额：'
-        sss = '当月消费：'
-        txt3 = '阿里云【gamegoing】账号余额'
-        txt3 = txt3 + ":\n" + test + alyun  + "\n" + sss +alyun3
-        return txt3
-
-
-def check_account3():
-    yymm = (time.strftime("%Y-%m", time.localtime()))
-    region_provider.add_endpoint('BssOpenApi', 'cn-hangzhou', 'business.aliyuncs.com')
-    clt = client.AcsClient('###',**','hangzhou')
-    request = QueryAccountBalanceRequest.QueryAccountBalanceRequest()
-    request2 = QueryMonthlyBillRequest()
-    request.set_accept_format("JSON")
-    request2.set_accept_format("JSON")
-    request2.set_BillingCycle(yymm)
-    result = clt.do_action_with_exception(request)
-    result2 = clt.do_action_with_exception(request2)
-    res_json = json.loads(str(result, encoding="utf-8"))
-    res_json2 = json.loads(str(result2, encoding="utf-8"))
-
-    if res_json["Code"] == "200":
-        alyun = res_json["Data"]["AvailableAmount"]
-        alyun2 = res_json2["Data"]["NewInvoiceAmount"]
-        alyun3 = str(alyun2)
-        test = '当前余额：'
-        sss = '当月消费：'
-        txt3 = '阿里云【lefeimobi】账号余额'
-        txt3 = txt3 + ":\n" + test + alyun  + "\n" + sss +alyun3
-        return txt3
-
-def check_account4():
-    yymm = (time.strftime("%Y-%m", time.localtime()))
-    region_provider.add_endpoint('BssOpenApi', 'cn-hangzhou', 'business.aliyuncs.com')
-    clt = client.AcsClient('###',**','hangzhou')
-    request = QueryAccountBalanceRequest.QueryAccountBalanceRequest()
-    request2 = QueryMonthlyBillRequest()
-    request.set_accept_format("JSON")
-    request2.set_accept_format("JSON")
-    request2.set_BillingCycle(yymm)
-    result = clt.do_action_with_exception(request)
-    result2 = clt.do_action_with_exception(request2)
-    res_json = json.loads(str(result, encoding="utf-8"))
-    res_json2 = json.loads(str(result2, encoding="utf-8"))
-
-    if res_json["Code"] == "200":
-        alyun = res_json["Data"]["AvailableAmount"]
-        alyun2 = res_json2["Data"]["NewInvoiceAmount"]
-        alyun3 = str(alyun2)
-        test = '当前余额：'
-        sss = '当月消费：'
-        txt3 = '阿里云【zwqing333@163.com】账号余额'
+        txt3 = '阿里云账号余额'
         txt3 = txt3 + ":\n" + test + alyun  + "\n" + sss +alyun3
         return txt3
 
 if __name__ == '__main__':
     wechatwork(check_account())
-    wechatwork(check_account2())
-    wechatwork(check_account3())
-    wechatwork(check_account4())
 
 
 
